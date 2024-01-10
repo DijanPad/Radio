13930c720fb7e17e361a1721313298cb87b54d9b
DijanPad <124375530+DijanPad@users.noreply.github.com>
2024-01-10 08:59:43 -0600

    a
    a

---
+++ Radio.java
@@ -0,0 +1,50 @@
+public class Radio {
+
+    private boolean encendido;
+    private String marca;
+    private int banda;
+    private float estacion;
+    private  int volume;
+
+
+    //metodos
+    public boolean isEncendido() {
+        return encendido;
+    }
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public int getBanda() {
+        return banda;
+    }
+    public void setBanda(int banda) {
+        this.banda = banda;
+    }
+    public float getEstacion() {
+        return estacion;
+    }
+    public void setEstacion(float estacion) {
+        this.estacion = estacion;
+    }
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    guardarEstacion(estacion,banda,indx){
+
+    }
+    recuoperarEstacion(index){
+        
+    }
+    
+
+
+}
